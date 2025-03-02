7dde429a70c3e8a3cdef35f95651162ef610a036
Farhan41229 <farhankhan@iut-dhaka.edu>
2025-03-02 13:42:36 +0600

    Added the cards hover effect and linked with the JS scripts

---
+++ scripts/tasks.js
@@ -0,0 +1,10 @@
+// Global variables
+let pending_tasks = 6;
+let completed_tasks = 0;
+let finished_tasks = 23;
+let task1_finished = false;
+let task2_finished = false;
+let task3_finished = false;
+let task4_finished = false;
+let task5_finished = false;
+let task6_finished = false;
